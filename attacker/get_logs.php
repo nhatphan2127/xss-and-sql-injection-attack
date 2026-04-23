7e4ee87dbde1c13a6b80a79c5ab81dacb2fbda57
NguyenNhat <nguyennhat@NguyenNhats-MacBook-Pro.local>
2026-04-23 13:23:07 +0700

    Mô tả thay đổi

--- attacker/get_logs.php
+++
@@ -1,9 +0,0 @@
-<?php
-        if (file_exists('stolen_cookies.txt') and filesize('stolen_cookies.txt') > 0) {
-            echo htmlspecialchars(file_get_contents('stolen_cookies.txt'));
-        } elseif (file_get_contents('stolen_cookies.txt') === '') {
-            echo "No cookies stolen yet...";
-        } else {
-            echo "Log file not found.";
-        }
-?>
